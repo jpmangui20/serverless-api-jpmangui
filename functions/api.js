f6e8e906065ec5fb4c523abdca1711c40a0e26f2
jpmangui20 <jpmangui9@gmail.com>
2024-06-10 10:44:57 +0800

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 
 // MongoDB URL
-const dbCloudUrl = 'mongodb+srv://jacoblaugo32:sarapmobenefer@cluster0.qwktp3q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const dbCloudUrl = 'mongodb+srv://jpmangui9:manguijaypee@cluster0.cmtonr1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const dbLocalUrl = 'mongodb://localhost:27017/blogPosts';
 
 app.use(cors());
